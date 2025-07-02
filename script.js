72c54a0cf960a17d80cd84144b8fe575af5c87c2
shxdowxxx <itzzzshxdow@gmail.com>
2025-07-02 07:48:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// Detect if the user is on a mobile device or not
+function isMobileDevice() {
+    return window.innerWidth <= 767;
+}
 
+// Toggle the mobile menu on button click
+function toggleMenu() {
+    var mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('active');
+}
+
+// Add event listener for resizing the window (to detect device changes)
+window.addEventListener('resize', function() {
+    if (isMobileDevice()) {
+        // Switch to mobile view
+        document.body.classList.add('mobile-view');
+        document.body.classList.remove('desktop-view');
+    } else {
+        // Switch to desktop view
+        document.body.classList.add('desktop-view');
+        document.body.classList.remove('mobile-view');
+    }
+});
+
+// Initial check on page load
+if (isMobileDevice()) {
+    document.body.classList.add('mobile-view');
+} else {
+    document.body.classList.add('desktop-view');
+}
